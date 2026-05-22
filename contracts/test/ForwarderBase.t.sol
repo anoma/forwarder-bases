1c8dc36321b9d400f1404ca1dca9e71ceaafbb38
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-05-22 09:11:27 +0200

    fix(test): use predicted address

--- contracts/test/ForwarderBase.t.sol
+++ contracts/test/ForwarderBase.t.sol
@@ -36,12 +36,16 @@ contract ForwarderBaseTest is Test {
     }
 
     function test_constructor_reverts_if_the_protocol_adapter_address_is_zero() public {
-        vm.expectRevert(ForwarderBase.ZeroNotAllowed.selector, address(_fwd));
+        address predicted = vm.computeCreateAddress(address(this), vm.getNonce(address(this)));
+
+        vm.expectRevert(ForwarderBase.ZeroNotAllowed.selector, predicted);
         new ForwarderExample({protocolAdapter: address(0), logicRef: _LOGIC_REF});
     }
 
     function test_constructor_reverts_if_the_logic_ref_is_zero() public {
-        vm.expectRevert(ForwarderBase.ZeroNotAllowed.selector, address(_fwd));
+        address predicted = vm.computeCreateAddress(address(this), vm.getNonce(address(this)));
+
+        vm.expectRevert(ForwarderBase.ZeroNotAllowed.selector, predicted);
         new ForwarderExample({protocolAdapter: _pa, logicRef: bytes32(0)});
     }
 
