44d56721bdd3042ef47d36c6532b8e64ecffbf79
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-06-08 12:40:08 +0200

    refactor: move initialization tests in different files

--- contracts/test/ForwarderBaseUpgradeable.t.sol
+++ contracts/test/ForwarderBaseUpgradeable.t.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {Initializable} from "@openzeppelin-contracts-5.6.1/proxy/utils/Initializable.sol";
 import {ReentrancyGuardTransient} from "@openzeppelin-contracts-5.6.1/utils/ReentrancyGuardTransient.sol";
-import {OwnableUpgradeable} from "@openzeppelin-contracts-upgradeable-5.6.1/access/OwnableUpgradeable.sol";
 import {Test} from "forge-std-1.16.1/src/Test.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades-0.4.1/src/Upgrades.sol";
 
 import {ForwarderBaseUpgradeable} from "../src/ForwarderBaseUpgradeable.sol";
-import {ERC1967ProxyUnsafe} from "./examples/ERC1967ProxyUnsafe.sol";
 import {
     ForwarderTargetExample,
     INPUT_VALUE,
@@ -43,43 +40,6 @@ contract ForwarderBaseUpgradeableUpgradeableTest is Test {
                 abi.encodeCall(ForwarderUpgradeableExample.initialize, (_pa, _LOGIC_REF, _PA_OWNER))
             )
         );
-    }
-
-    function test_constructor_disables_initializers_of_the_implementation_contract() public {
-        ForwarderUpgradeableExample impl = new ForwarderUpgradeableExample();
-        vm.expectRevert(Initializable.InvalidInitialization.selector, address(impl));
-
-        impl.initialize({protocolAdapter: _pa, logicRef: _LOGIC_REF, owner: _FORWARDER_OWNER});
-    }
-
-    function test_initialize_reverts_if_the_protocol_adapter_address_is_zero() public {
-        ForwarderUpgradeableExample uninitializedFwd = ForwarderUpgradeableExample(
-            address(new ERC1967ProxyUnsafe(address(new ForwarderUpgradeableExample()), ""))
-        );
-
-        vm.expectRevert(ForwarderBaseUpgradeable.ZeroNotAllowed.selector, address(uninitializedFwd));
-        uninitializedFwd.initialize({protocolAdapter: address(0), logicRef: _LOGIC_REF, owner: _FORWARDER_OWNER});
-    }
-
-    function test_initialize_reverts_if_the_logic_ref_is_zero() public {
-        ForwarderUpgradeableExample uninitializedFwd = ForwarderUpgradeableExample(
-            address(new ERC1967ProxyUnsafe(address(new ForwarderUpgradeableExample()), ""))
-        );
-
-        vm.expectRevert(ForwarderBaseUpgradeable.ZeroNotAllowed.selector, address(uninitializedFwd));
-        uninitializedFwd.initialize({protocolAdapter: _pa, logicRef: bytes32(0), owner: _FORWARDER_OWNER});
-    }
-
-    function test_initialize_reverts_if_the_owner_is_zero() public {
-        ForwarderUpgradeableExample uninitializedFwd = ForwarderUpgradeableExample(
-            address(new ERC1967ProxyUnsafe(address(new ForwarderUpgradeableExample()), ""))
-        );
-
-        vm.expectRevert(
-            abi.encodeWithSelector(OwnableUpgradeable.OwnableInvalidOwner.selector, address(0)),
-            address(uninitializedFwd)
-        );
-        uninitializedFwd.initialize({protocolAdapter: _pa, logicRef: _LOGIC_REF, owner: address(0)});
     }
 
     function test_forwardCall_reverts_if_the_pa_is_not_the_caller() public {
