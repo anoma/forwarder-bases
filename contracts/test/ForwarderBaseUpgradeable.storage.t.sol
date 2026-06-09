287d2735429b0ac6f22a4f323d2f6a6e4759d2f5
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-06-09 12:04:31 +0200

    refactor: fix storage test warnings

--- contracts/test/ForwarderBaseUpgradeable.storage.t.sol
+++ contracts/test/ForwarderBaseUpgradeable.storage.t.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {SlotDerivation} from "@openzeppelin-contracts-5.6.1/utils/SlotDerivation.sol";
 import {Test} from "forge-std-1.16.1/src/Test.sol";
 
 import {ForwarderBaseUpgradeable} from "../src/ForwarderBaseUpgradeable.sol";
 
 contract ForwarderBaseUpgradeableStorageTest is Test, ForwarderBaseUpgradeable {
-    function test_storage_slot() public {
-        assertEq(_FORWARDER_BASE_STORAGE_LOCATION, bytes32(erc7201("anoma.storage.ForwarderBase")));
+    function test_storage_slot() public pure {
+        assertEq(_FORWARDER_BASE_STORAGE_LOCATION, SlotDerivation.erc7201Slot("anoma.storage.ForwarderBase"));
     }
 
-    function _forwardCall(bytes calldata input) internal override returns (bytes memory output) {
+    function _forwardCall(bytes calldata input) internal pure override returns (bytes memory output) {
         (input);
         output = "";
     }
