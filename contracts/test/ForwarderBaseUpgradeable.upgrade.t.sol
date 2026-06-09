74ce4df6fd9d4c9591399629ef68b816d041a436
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-06-09 10:55:02 +0200

    chore: homogenize test file licenses

--- contracts/test/ForwarderBaseUpgradeable.upgrade.t.sol
+++ contracts/test/ForwarderBaseUpgradeable.upgrade.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
 import {UUPSUpgradeable} from "@openzeppelin-contracts-5.6.1/proxy/utils/UUPSUpgradeable.sol";
