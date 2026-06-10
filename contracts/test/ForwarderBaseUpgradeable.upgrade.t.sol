605882a92eb6ea2a30016906059474d338e33e0e
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-06-10 10:33:38 +0200

    test: check events are emitted correctly

--- contracts/test/ForwarderBaseUpgradeable.upgrade.t.sol
+++ contracts/test/ForwarderBaseUpgradeable.upgrade.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {IERC1967} from "@openzeppelin-contracts-5.6.1/interfaces/IERC1967.sol";
+import {Initializable} from "@openzeppelin-contracts-5.6.1/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin-contracts-5.6.1/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin-contracts-upgradeable-5.6.1/access/OwnableUpgradeable.sol";
 import {Test} from "forge-std-1.16.1/src/Test.sol";
@@ -19,14 +21,14 @@ contract ForwarderBaseUpgradeableUpgradeTest is Test {
     bytes32 internal constant _LOGIC_REF = bytes32(type(uint256).max);
 
     address internal _pa;
-    address internal _fwd;
+    address internal _fwdProxy;
     ForwarderTargetExample internal _tgt;
 
     function setUp() public {
         _pa = address(new ProtocolAdapterMock(_PA_OWNER));
 
         _tgt = new ForwarderTargetExample();
-        _fwd = Upgrades.deployUUPSProxy(
+        _fwdProxy = Upgrades.deployUUPSProxy(
             "ForwarderUpgradeableExample.sol:ForwarderUpgradeableExample",
             abi.encodeCall(ForwarderUpgradeableExample.initialize, (_pa, _LOGIC_REF, _FORWARDER_OWNER))
         );
@@ -37,7 +39,8 @@ contract ForwarderBaseUpgradeableUpgradeTest is Test {
         address newImpl = Upgrades.prepareUpgrade("ForwarderUpgradeableExample.sol:ForwarderUpgradeableExampleV2", opts);
 
         vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, address(this)));
-        UUPSUpgradeable(_fwd).upgradeToAndCall(newImpl, abi.encodeCall(ForwarderUpgradeableExampleV2.reinitialize, ()));
+        UUPSUpgradeable(_fwdProxy)
+            .upgradeToAndCall(newImpl, abi.encodeCall(ForwarderUpgradeableExampleV2.reinitialize, ()));
     }
 
     function test_upgrade_upgrades_to_the_same_version() public {
@@ -46,10 +49,25 @@ contract ForwarderBaseUpgradeableUpgradeTest is Test {
         // to the deploy.
         vm.startPrank(_FORWARDER_OWNER);
         Upgrades.upgradeProxy(
-            _fwd,
+            _fwdProxy,
             "ForwarderUpgradeableExample.sol:ForwarderUpgradeableExampleV2",
             abi.encodeCall(ForwarderUpgradeableExampleV2.reinitialize, ())
         );
         vm.stopPrank();
+    }
+
+    function test_upgradeToAndCall_emits_the_Upgraded_and_Initialized_events() public {
+        Options memory opts;
+        address newImpl = Upgrades.prepareUpgrade("ForwarderUpgradeableExample.sol:ForwarderUpgradeableExampleV2", opts);
+
+        vm.expectEmit(address(_fwdProxy));
+        emit IERC1967.Upgraded({implementation: newImpl});
+
+        vm.expectEmit(_fwdProxy);
+        emit Initializable.Initialized({version: 2});
+
+        vm.prank(_FORWARDER_OWNER);
+        UUPSUpgradeable(_fwdProxy)
+            .upgradeToAndCall(newImpl, abi.encodeCall(ForwarderUpgradeableExampleV2.reinitialize, ()));
     }
 }
